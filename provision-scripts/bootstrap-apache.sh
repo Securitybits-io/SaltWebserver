7f38695be4ba96730b07622d7ba89776d8fa81a3
Christoffer Claesson <Christoffer.Claesson@Securitybits.io>
2019-02-17 16:08:21 +0100

    removed provisioned bashrc

--- provision-scripts/bootstrap-apache.sh
+++ provision-scripts/bootstrap-apache.sh
@@ -3,10 +3,6 @@ hostname apache
 mkdir /etc/salt
 echo 'apache' > /etc/salt/minion_id
 
-# First copy bashrc file that enables color text, I hate BW :(
-cp /vagrant/provision-scripts/deploy-file/bashrc /home/vagrant/.bashrc
-cp /vagrant/provision-scripts/deploy-file/bashrc /root/.bashrc
-
 #echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
 #wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
 apt-get update
--- provision-scripts/bootstrap-salt.sh
+++ provision-scripts/bootstrap-salt.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 hostname salt
 
-# First copy bashrc file that enables color text, I hate BW :(
-cp /vagrant/provision-scripts/deploy-file/bashrc /home/vagrant/.bashrc
-cp /vagrant/provision-scripts/deploy-file/bashrc /root/.bashrc
-
 #echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | sudo tee /etc/apt/sources.list.d/saltstack.list
 #wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | sudo apt-key add -
 apt-get update
